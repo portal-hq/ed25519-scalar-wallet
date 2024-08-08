961b9079df78332a634a90aaf8c9fec0ef824720
David Scrobonia <davidscrobonia@gmail.com>
2024-08-07 18:44:05 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  getPublicKey,
+  getSolBalance,
+  signAndSendSolTransaction,
+} from "./libs/solana";
+
+function App() {
+  const [scalarKey, setScalarKey] = useState<string>("");
+  const [walletAddress, setWalletAddress] = useState<string | null>(null);
+  const [walletBalance, setWalletBalance] = useState<number | null>(null);
+  const [recipient, setRecipient] = useState<string>("");
+  const [transferAmount, setTransferAmount] = useState<number>(0);
+  const [txhash, setTxhash] = useState<string | null>(null);
+
+  /*
+   * Import the Solana wallet using the ed25519 scalar private key
+   * and fetch the balance.
+   */
+  const importSolanaWallet = async () => {
+    const address = await getPublicKey(scalarKey);
+    setWalletAddress(address);
+
+    const solBalance = await getSolBalance(address);
+    setWalletBalance(solBalance);
+  };
+
+  /*
+   * Transfer SOL to the recipient address.
+   */
+  const transferSol = async () => {
+    if (!walletAddress) {
+      return;
+    }
+
+    const txhash = await signAndSendSolTransaction(
+      scalarKey,
+      walletAddress,
+      recipient,
+      transferAmount
+    );
+
+    setTxhash(txhash);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {!walletAddress && (
+          <>
+            {/* Form to input ed25119 scalar private key and an import button */}
+            <p>Enter ed25519-scalar private key</p>
+            <input
+              type="text"
+              placeholder="ed25519 scalar private key"
+              onChange={(e) => setScalarKey(e.target.value)}
+              value={scalarKey}
+            />
+            <button onClick={() => importSolanaWallet()}>Import</button>
+          </>
+        )}
+
+        {/* Show the Solana address */}
+        {walletAddress && (
+          <>
+            {/* Show the Solana address and link the address to the Solana explorer */}
+            <h1>Wallet Details</h1>
+            <p>
+              Solana Address:{" "}
+              <a
+                className="App-link"
+                href={`https://explorer.solana.com/address/${walletAddress}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {walletAddress}
+              </a>
+            </p>
+
+            {/* Show the Solana balance */}
+            <p>
+              SOL Balance:{" "}
+              {walletBalance !== null ? walletBalance : "Loading..."}
+            </p>
+
+            {/* Form to transfer PYUSD to another account */}
+            <h1>Transfer SOL</h1>
+            <label htmlFor="recipient">Recipient Address</label>
+            <input
+              type="text"
+              placeholder="Recipient Address"
+              id="recipient"
+              onChange={(e) => setRecipient(e.target.value)}
+              value={recipient}
+            />
+
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              placeholder="Amount"
+              id="amount"
+              onChange={(e) => setTransferAmount(parseFloat(e.target.value))}
+              value={transferAmount}
+            />
+            <button onClick={() => transferSol()}>Transfer</button>
+            {txhash && (
+              <p>
+                Transaction Hash:{" "}
+                <a
+                  className="App-link"
+                  href={`https://explorer.solana.com/tx/${txhash}?cluster=devnet`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {txhash}
+                </a>
+              </p>
+            )}
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/libs/solana/index.ts
@@ -0,0 +1,116 @@
+/*
+ * Helper functions for interacting with the Solana blockchain.
+ */
+
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import * as bs58 from "bs58";
+import { getPublicKeyAsync, signAsync } from "../ed25119-scalar";
+
+/*
+ * Fetches the SOL balance of the given address.
+ */
+export async function getSolBalance(address: string): Promise<number> {
+  return await fetch("https://api.devnet.solana.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: 1,
+      method: "getBalance",
+      params: [address],
+    }),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((body) => {
+      const lamports = body.result.value;
+      const sol = lamports / LAMPORTS_PER_SOL;
+      return sol;
+    })
+    .catch((err) => {
+      console.error(err);
+      return 0;
+    });
+}
+
+/*
+ * Derives a Solana public key from the given private key.
+ */
+export async function getPublicKey(privateKey: string): Promise<string> {
+  // Convert base58 encoded private key to bytes
+  const privateKeyBytes = bs58.default.decode(privateKey);
+  if (privateKeyBytes.length !== 32) {
+    throw new Error("Invalid private key length");
+  }
+
+  // Convert bytes to hex string
+  const privateKeyHex = Buffer.from(privateKeyBytes).toString("hex");
+
+  // Get and print Solana public key
+  const result = await getPublicKeyAsync(privateKeyHex);
+  const solanaAddress = bs58.default.encode(result);
+
+  return solanaAddress;
+}
+
+/*
+ * Execute transfer of SOL to another owner with the Ed25519 Key.
+ */
+export async function signAndSendSolTransaction(
+  scalarKey: string,
+  fromAddress: string,
+  toAddress: string,
+  amount: number
+): Promise<string> {
+  // Create public keys from address strings
+  const fromPublicKey = new PublicKey(fromAddress);
+  const toPublicKey = new PublicKey(toAddress);
+
+  // prep the private key hex
+  const privateKeyHex = Buffer.from(bs58.default.decode(scalarKey)).toString(
+    "hex"
+  );
+
+  const connection = new Connection("https://api.devnet.solana.com");
+  const { blockhash } = await connection.getLatestBlockhash("finalized");
+  let tx = new Transaction({
+    recentBlockhash: blockhash,
+    feePayer: fromPublicKey,
+  }).add(
+    SystemProgram.transfer({
+      fromPubkey: fromPublicKey,
+      toPubkey: toPublicKey,
+      lamports: LAMPORTS_PER_SOL * amount,
+    })
+  );
+
+  // Serialize the transaction to a Buffer
+  const serializedMessage = tx.serializeMessage();
+
+  // Convert the serialized transaction to a hex string
+  const serializedHex = Buffer.from(serializedMessage).toString("hex");
+  const transactionSignature = await signAsync(serializedHex, privateKeyHex);
+
+  // Convert uint8 array to Buffer
+  const signatureBuffer = Buffer.from(transactionSignature);
+  console.log("Unsigned transaction: ", tx);
+  tx.addSignature(fromPublicKey, signatureBuffer);
+  console.log("Signed transaction: ", tx);
+  if (!tx.verifySignatures()) {
+    throw new Error("Signature verification failed");
+  }
+
+  const rawTransaction = tx.serialize();
+  const hash = await connection.sendRawTransaction(rawTransaction);
+
+  return hash;
+}
