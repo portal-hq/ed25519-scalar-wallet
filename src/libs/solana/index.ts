26e0306cfc4437ca5f52091974e158681bb4a1b2
David Scrobonia <davidscrobonia@gmail.com>
2024-09-12 15:52:38 +0800

    add transfer function for SPL tokens

--- src/libs/solana/index.ts
+++ src/libs/solana/index.ts
@@ -6,14 +6,27 @@ import {
   Connection,
   LAMPORTS_PER_SOL,
   PublicKey,
+  SendTransactionError,
   SystemProgram,
   Transaction,
+  TransactionMessage,
+  VersionedTransaction,
 } from "@solana/web3.js";
 import * as bs58 from "bs58";
 import { getPublicKeyAsync, signAsync } from "../ed25119-scalar";
+import {
+  createAssociatedTokenAccountInstruction,
+  createTransferCheckedInstruction,
+  getAssociatedTokenAddress,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
 
-const DEVNET_URL = "https://api.devnet.solana.com";
-const MAINNET_URL = "https://api.mainnet-beta.solana.com";
+const DEVNET_URL =
+  process.env.REACT_APP_SOLANA_DEVNET_RPC_URL ||
+  "https://api.devnet.solana.com";
+const MAINNET_URL =
+  process.env.REACT_APP_SOLANA_MAINNET_RPC_URL ||
+  "https://api.mainnet-beta.solana.com";
 
 const getNetworkUrl = (useMainnet: boolean) => {
   return useMainnet ? MAINNET_URL : DEVNET_URL;
@@ -113,13 +126,16 @@ export async function signAndSendSolTransaction(
 
   // Convert the serialized transaction to a hex string
   const serializedHex = Buffer.from(serializedMessage).toString("hex");
+
+  // Sign the serialized transaction
   const transactionSignature = await signAsync(serializedHex, privateKeyHex);
 
-  // Convert uint8 array to Buffer
+  // Add the signature to the transaction
   const signatureBuffer = Buffer.from(transactionSignature);
-  console.log("Unsigned transaction: ", tx);
   tx.addSignature(fromPublicKey, signatureBuffer);
   console.log("Signed transaction: ", tx);
+
+  // Verify the transaction
   if (!tx.verifySignatures()) {
     throw new Error("Signature verification failed");
   }
@@ -128,4 +144,143 @@ export async function signAndSendSolTransaction(
   const hash = await connection.sendRawTransaction(rawTransaction);
 
   return hash;
+}
+
+/*
+ * Execute transfer of an SPL-22 token to another owner.
+ */
+export async function signAndSendSPLTransferTransaction(
+  scalarKey: string,
+  sender: string,
+  recipient: string,
+  amount: number,
+  tokenAddress: string,
+  useMainnet: boolean
+): Promise<string | null> {
+  const network = getNetworkUrl(useMainnet);
+
+  // prep the private key hex
+  const privateKeyHex = Buffer.from(bs58.default.decode(scalarKey)).toString(
+    "hex"
+  );
+
+  // Create connection to the network
+  const connection = new Connection(network);
+
+  const tokenPublicKey = new PublicKey(tokenAddress);
+  const PYUSD_DECIMALS = 6;
+
+  // Sender & receiver PublicKeys from address strings
+  const senderPublicKey = new PublicKey(sender);
+  const recipientPublicKey = new PublicKey(recipient);
+
+  // Instructions array to hold the transaction instructions
+  const instructions = [];
+
+  // Each SPL token has an associated token account for each owner
+  // Check if the sender address has an associated token account for PYUSD
+  const senderTokenAddress = await getAssociatedTokenAddress(
+    tokenPublicKey,
+    senderPublicKey,
+    false,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const senderAccountInfo = await connection.getAccountInfo(senderTokenAddress);
+  if (!senderAccountInfo) {
+    // If there is no senderAccountInfo then the sender does not have any
+    // PYUSD to send.
+    console.error(
+      `Sender ${sender} does not have a token account for PYUSD (${tokenPublicKey.toString()}). They likely do not any PYUSD to send. Get some PYUSD before trying to send.`
+    );
+    return null;
+  }
+
+  // Check if the recipient address has an associated token account for PYUSD
+  const receiverTokenAddress = await getAssociatedTokenAddress(
+    tokenPublicKey,
+    recipientPublicKey,
+    false,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const receiverAccountInfo = await connection.getAccountInfo(
+    receiverTokenAddress
+  );
+  if (!receiverAccountInfo) {
+    // If there is no receiverAccountInfo then we need to create and pay for
+    // a token account for the recipient before we can transfer PYUSD to them.
+    console.log(
+      `Creating new token account at ${receiverTokenAddress} for ${recipient} on ${tokenPublicKey.toString()} paid for by ${sender}`
+    );
+    const createTokenAccountIx = createAssociatedTokenAccountInstruction(
+      senderPublicKey, // Payer
+      receiverTokenAddress, // Associated token account address
+      recipientPublicKey, // Owner of the associated token account
+      tokenPublicKey,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    // Add the create token account instruction to the instructions array
+    // so we can execute all instructions in one transaction.
+    instructions.push(createTokenAccountIx);
+  }
+
+  // Fetch recent blockhash
+  const blockhash = await connection.getLatestBlockhash("finalized");
+
+  // Create the transfer instruction to send PYUSD from sender
+  // to recipient
+  const transferInstruction = createTransferCheckedInstruction(
+    senderTokenAddress, // sender token account
+    tokenPublicKey, // mint PublicKey
+    receiverTokenAddress, // receiver token account
+    senderPublicKey, // owner PublicKey
+    amount * Math.pow(10, PYUSD_DECIMALS), // amount in minimum units
+    PYUSD_DECIMALS, // decimals
+    [], // signers - empty because we're going to be signing when we submit
+    TOKEN_2022_PROGRAM_ID // programId
+  );
+
+  // Add the create token account instruction to the instructions array
+  // so we can execute all instructions in one transaction.
+  instructions.push(transferInstruction);
+
+  // Create the transaction message with the payer and latest blockhash
+  const message = new TransactionMessage({
+    payerKey: senderPublicKey,
+    recentBlockhash: blockhash.blockhash,
+    instructions,
+  });
+
+  // Create the versioned transaction with the message.
+  // CompileToV0Message will convert the message to a versioned message
+  // https://solana.com/docs/advanced/versions#notes
+  const transaction = new VersionedTransaction(message.compileToV0Message());
+  console.log("Unsigned versioned transaction: ", transaction);
+
+  // Serialized the transaction and properly encode it to be
+  // signed by Portal
+  const serializedTransaction = transaction.message.serialize();
+
+  // Convert the serialized transaction to a hex string
+  const serializedHex = Buffer.from(serializedTransaction).toString("hex");
+
+  // Sign the serialized transaction
+  const transactionSignature = await signAsync(serializedHex, privateKeyHex);
+
+  // Add the signature to the transaction
+  const signatureBuffer = Buffer.from(transactionSignature);
+  transaction.addSignature(senderPublicKey, signatureBuffer);
+
+  const rawTransaction = transaction.serialize();
+
+  try {
+    const hash = await connection.sendRawTransaction(rawTransaction);
+    return hash;
+  } catch (e) {
+    console.log(e);
+    (e as SendTransactionError).getLogs(connection);
+  }
+  return null;
 }
