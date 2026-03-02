feab130a713732df7fd89c2aef6d7f81c1bdbc1c
Leo Suarez Arnaldi <leonel.suarezarnaldi@gleni.tech>
2026-03-02 17:56:05 -0300

    fix: :bug: formatting

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,24 @@
 const webpack = require("webpack");
 
-module.exports = {
-  webpack: function override(config, env) {
-    config.resolve.fallback = {
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      buffer: require.resolve("buffer"),
-      path: require.resolve("path-browserify"),
-      vm: require.resolve("vm-browserify"),
-    };
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify/browser"),
+    buffer: require.resolve("buffer"),
+    path: require.resolve("path-browserify"),
+    vm: require.resolve("vm-browserify"),
+  };
 
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-        Buffer: ["buffer", "Buffer"],
-      })
-    );
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
 
-    return config;
-  },
+  return config;
 };
